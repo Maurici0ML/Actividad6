1bf5383fe1a9d583966b16df43f59c6d8c7144be
Diego Hernandez <redbandito@outlook.com>
2021-05-24 16:22:44 -0500

    Feat: Animations

--- script/events.js
+++ script/events.js
@@ -1,18 +1,28 @@
-$(window).load(()=>{
+$(window).on("load", ()=>{
+//Animaciones 
+    slimeSquare1=$("#Slime1");
+    slimeSquare2=$("#Slime2");
+    slimeSquare3=$("#Slime3");
     anim1=$("#animation1");
     anim2=$("#animation2");
     anim3=$("#animation3");
 
     anim1.on("click", ()=>{
-        console.log("animacion1");
-        anim1.addClass("move1");
+        console.log("Saltar");
+        slimeSquare1.toggleClass("move1");
+        slimeSquare2.toggleClass("move1");
+        slimeSquare3.toggleClass("move1");
     });
     anim2.on("click", ()=>{
-        console.log("animacion2");
-        anim1.addClass("move2");
+        console.log("Inflarse");
+        slimeSquare1.toggleClass("move2");
+        slimeSquare2.toggleClass("move2");
+        slimeSquare3.toggleClass("move2");
     });
     anim3.on("click", ()=>{
-        console.log("animacion3");
-        anim1.addClass("move3");
+        console.log("Acariciar");
+        slimeSquare1.toggleClass("move3");
+        slimeSquare2.toggleClass("move3");
+        slimeSquare3.toggleClass("move3");
     });
 });
