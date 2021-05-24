965bb6a8348df419281d08fe9cdd11293b3d7925
Maurici0ML <alanmauriciomoraleslopez@gmail.com>
2021-05-24 16:25:00 -0500

    FEAT: cambio de colores

--- script/events.js
+++ script/events.js
@@ -1,8 +1,41 @@
-$(window).load(()=>{
+$(window).on('load',()=>{
     anim1=$("#animation1");
     anim2=$("#animation2");
     anim3=$("#animation3");
 
+    let btnRojo=$("#rojo");
+    let btnRosa=$("#rosa");
+    let btnVerde=$("#verde");
+    let btnNaranja=$("#naranja");
+    let slime=$(".bodySlime");
+
+    //Seccion donde se detecta el click para los colores del slime
+    btnRojo.click(()=>{
+        for(let i=0; i<2; i++){
+            slime.css("background-color", "#B5282D")[i]; 
+            slime.css("border-color", "#8C0000")[i]; 
+        }
+    });
+    btnRosa.click(()=>{
+        for(let i=0; i<2; i++){
+            slime.css("background-color", "#CE77BF")[i];
+            slime.css("border-color", "#e6a5da")[i];
+        }
+    });
+    btnVerde.click(()=>{
+        for(let i=0; i<2; i++){
+            slime.css("background-color", "#4DBA30")[i];
+            slime.css("border-color", "#3D9126")[i];
+        }
+    });
+    btnNaranja.click(()=>{
+        for(let i=0; i<2; i++){
+            slime.css("background-color", "#D17E2D")[i];
+            slime.css("border-color", "#C45C00")[i];
+        }
+    });
+
+    //Seccion donde se detecta el click para las animaciones.
     anim1.on("click", ()=>{
         console.log("animacion1");
         anim1.addClass("move1");
